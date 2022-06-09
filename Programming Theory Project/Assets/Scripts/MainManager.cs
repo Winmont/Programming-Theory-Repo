e233c3fb7a5d5b5a64affb947bf7bfd4cb1f02a9
Winmont <106991316+Winmont@users.noreply.github.com>
2022-06-09 20:27:44 +0200

    titlescreen finalized
    Added script funtionality to the start game and quit game button and input field.

--- Programming Theory Project/Assets/Scripts/MainManager.cs
+++ Programming Theory Project/Assets/Scripts/MainManager.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainManager : MonoBehaviour
 {
+
+    [SerializeField] InputField inputField;
+
+    private static MainManager m_instance;
+    public static MainManager instance
+    {
+        get
+        {
+            return m_instance;
+        }
+
+    }
+
+    private string playerName;
+
+    private void Awake()
+    {
+        if (m_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        m_instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void UpdatePlayerName()
+    {
+        playerName = inputField.text;
     }
 }
